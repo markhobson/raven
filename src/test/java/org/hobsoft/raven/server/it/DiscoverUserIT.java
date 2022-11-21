732dba63c8a18d4a9ae37dad3d5c178045255bbb
Mark Hobson <markhobson@gmail.com>
2022-11-21 16:12:45 +0000

    Extract integration test annotation

--- src/test/java/org/hobsoft/raven/server/it/DiscoverUserIT.java
+++ src/test/java/org/hobsoft/raven/server/it/DiscoverUserIT.java
@@ -4,10 +4,7 @@ import org.hobsoft.raven.server.User;
 import org.hobsoft.raven.server.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
@@ -15,9 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@TestPropertySource("classpath:application-test.properties")
+@IntegrationTest
 public class DiscoverUserIT
 {
 	@MockBean
---
+++ src/test/java/org/hobsoft/raven/server/it/IntegrationTest.java
@@ -0,0 +1,24 @@
+package org.hobsoft.raven.server.it;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+@Documented
+@Inherited
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("classpath:application-test.properties")
+public @interface IntegrationTest
+{
+}
