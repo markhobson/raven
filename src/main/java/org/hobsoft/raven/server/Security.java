9b6787352ff0b4b0143e3b4212f1881923546e99
Mark Hobson <markhobson@gmail.com>
2022-12-19 10:05:30 +0000

    Document security vocabulary

--- src/main/java/org/hobsoft/raven/server/Security.java
+++ src/main/java/org/hobsoft/raven/server/Security.java
@@ -2,6 +2,11 @@ package org.hobsoft.raven.server;
 
 import java.net.URI;
 
+/**
+ * Security Vocabulary for linked data.
+ *
+ * @see <a href="https://web-payments.org/vocabs/security">The Security Vocabulary</a>
+ */
 public final class Security
 {
 	private Security()
@@ -11,6 +16,11 @@ public final class Security
 	
 	public static final URI CONTEXT = URI.create("https://w3id.org/security/v1");
 	
+	/**
+	 * Public key property.
+	 * 
+	 * @see <a href="https://web-payments.org/vocabs/security#publicKey">publicKey</a>
+	 */
 	public record PublicKey(URI id, URI owner, String publicKeyPem)
 	{
 	}
