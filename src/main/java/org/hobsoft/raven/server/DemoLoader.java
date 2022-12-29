40a7c0fb39781af4f3e151bb54863102bb03c0bf
Mark Hobson <markhobson@gmail.com>
2022-12-29 17:26:17 +0000

    Move demo loader into profile
    Avoids it being activated when running ITs.

--- src/main/java/org/hobsoft/raven/server/DemoLoader.java
+++ src/main/java/org/hobsoft/raven/server/DemoLoader.java
@@ -7,9 +7,11 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("demo")
 public class DemoLoader
 {
 	private static final Logger LOG = LoggerFactory.getLogger(DemoLoader.class);
